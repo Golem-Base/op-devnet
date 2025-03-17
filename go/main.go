7166e540152eddcba38c9498f0550932aa059ca8
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-03-17 14:11:27 +0000

    initialised and wrote checkL1 script

---
+++ go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Golem-Base/op.nix/go/cmd"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+
+	app := &cli.App{
+		Name:  "op.nix",
+		Usage: "Helper utilities for devnet",
+
+		Commands: []*cli.Command{
+			cmd.CheckL1Command,
+		},
+	}
+
+	// Run the CLI
+	err := app.Run(os.Args)
+	if err != nil {
+		slog.Error("Error", app.Name, err)
+	}
+}
