77bf5b15d18c68cf7825e9248fc8fe677f0d7d13
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2025-03-26 12:40:43 +0100

    fix: invalid packages spec for shell.nix

--- shell.nix
+++ shell.nix
@@ -22,14 +22,15 @@
           process-compose
         ])
         ++ (with self'.packages; [
-          packages.eth2-testnet-genesis
-          packages.kurtosis
-          packages.op-batcher-v1_11_4
-          packages.op-deployer-v0_2_0_rc1
-          packages.op-geth-v1_101500_1
-          packages.op-node-v1_11_2
-          packages.op-proposer-v1_10_0
-        ]) ++ (with inputs; [
+          eth2-testnet-genesis
+          kurtosis
+          op-batcher-v1_11_4
+          op-deployer-v0_2_0_rc1
+          op-geth-v1_101500_1
+          op-node-v1_11_2
+          op-proposer-v1_10_0
+        ])
+        ++ (with inputs; [
           withdrawer.packages.${system}.default
         ]);
     };
